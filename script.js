119e3bb6addb4b42a7a975814712ffe8f922e16b
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-04-16 18:23:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ var student = {
                 sclass : "VI", 
                 rollno : 12 
                } 
+Object.prototype.getKeys = function() {
+return Object.keys(this);
+};
+const keys = student.getKeys();
+
+console.log(keys);
